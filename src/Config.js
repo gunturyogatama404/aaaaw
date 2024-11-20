5581e507f88dc00665703e04fca8b4165363f48b
gunturyogatama404 <99540625+gunturyogatama404@users.noreply.github.com>
2024-11-20 16:59:37 +0700

    Update Config.js

--- src/Config.js
+++ src/Config.js
@@ -1,8 +1,9 @@
 class Config {
   constructor() {
-    this.ipCheckURL = 'https://ipinfo.io/json';
+    this.ipCheckURL = 'https://api.ipify.org?format=json';
     this.wssList = ['proxy.wynd.network:4444', 'proxy.wynd.network:4650'];
-    this.retryInterval = 20000;
+    this.retryInterval = 5000;
+    this.proxyDelay = 1000,
     this.wssHost =
       this.wssList[Math.floor(Math.random() * this.wssList.length)];
   }
